ead5a6586406a84b688e12e9a65ee85157450285
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-01-28 12:41:02 +0800

    added Payment model, changed to lowercase for student_tutor_session.py and tutor_session.py

---
+++ models/payment.py
@@ -0,0 +1,11 @@
+import datetime
+import peewee as pw
+from models.base_model import BaseModel 
+from models.student_tutor_session import Student_tutor_session
+
+class Payment(BaseModel):
+    # status = Pending, Complete, Error
+    student_tutor_session = pw.ForeignKeyField(Student_tutor_session, backref="student_tutor_sessions")
+    amount = pw.DecimalField(null=True)
+    status = pw.CharField(null=True) 
+    status_timestamp = pw.DateTimeField(default=datetime.datetime.now)
