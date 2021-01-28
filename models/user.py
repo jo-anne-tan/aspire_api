47ecbab7c924678f2165190469d1cf9f118f8738
Syakir Wong <mohdsyakir9997@gmail.com>
2021-01-28 12:31:00 +0800

    commit files

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 import re
 import peewee as pw
 from flask_login import UserMixin
-from models.base_model import BaseModel 
+from models.base_model import BaseModel
 from werkzeug.security import generate_password_hash
 
 class User(BaseModel,UserMixin):
@@ -13,17 +13,17 @@ class User(BaseModel,UserMixin):
     password=pw.CharField(null=True)
 
     # validations
-     def validate(self):
+    def validate(self):
         self.duplicate_check()
 
         if self.password:
             if not (self.password[0:19] == "pbkdf2:sha256:50000"): # if password is not changed
-                self.password_check() 
+                self.password_check()
 
     def duplicate_check(self):
         duplicate_email =  User.get_or_none(User.email==self.email)
-        
-        if duplicate_email: 
+
+        if duplicate_email:
             if not duplicate_email.id==self.id: #if the id is not your own
                 self.errors.append("Email is already taken. Please try again.")
 
@@ -31,9 +31,9 @@ class User(BaseModel,UserMixin):
         error_flag = False
 
         special_char = re.search('[\W]', self.password)
-        lowercase = re.search('[a-z]',self.password) 
-        uppercase = re.search('[A-Z]',self.password) 
-        number = re.search('[0-9]',self.password) 
+        lowercase = re.search('[a-z]',self.password)
+        uppercase = re.search('[A-Z]',self.password)
+        number = re.search('[0-9]',self.password)
 
         if len(self.password) <6:
             self.errors.append("Password must be longer than 6 characters")
