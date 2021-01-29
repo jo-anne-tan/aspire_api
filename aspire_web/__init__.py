fae5702cf3e86a380ae2babe8c86a5e5c7f6a6ac
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-01-29 15:58:09 +0800

    tested create student_tutor_session isntance

--- aspire_web/__init__.py
+++ aspire_web/__init__.py
@@ -6,6 +6,7 @@ from aspire_web.blueprints.tutors.views import tutors_blueprint
 from aspire_web.blueprints.students.views import students_blueprint
 from aspire_web.blueprints.subjects.views import subjects_blueprint
 from aspire_web.blueprints.tutor_sessions.views import tutor_sessions_blueprint
+from aspire_web.blueprints.student_tutor_sessions.views import student_tutor_sessions_blueprint
 
 assets = Environment(app)
 assets.register(bundles)
@@ -14,6 +15,7 @@ app.register_blueprint(tutors_blueprint, url_prefix="/tutors")
 app.register_blueprint(students_blueprint, url_prefix="/students")
 app.register_blueprint(subjects_blueprint, url_prefix="/subjects")
 app.register_blueprint(tutor_sessions_blueprint, url_prefix="/tutor_sessions")
+app.register_blueprint(student_tutor_sessions_blueprint, url_prefix="/student_tutor_sessions")
 
 
 @app.route('/')
