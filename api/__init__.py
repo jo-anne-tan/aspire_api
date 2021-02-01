c6fd15872b878ef2220e256b9a096a9f208b1f6c
Syakir Wong <mohdsyakir9997@gmail.com>
2021-02-01 17:34:22 +0800

    add new payment api, update payment status student_tutor_sesion api

--- api/__init__.py
+++ api/__init__.py
@@ -10,10 +10,11 @@ from api.blueprints.login.views import login_api_blueprint
 from api.blueprints.subjects.views import subjects_api_blueprint
 from api.blueprints.tutor_sessions.views import tutor_sessions_api_blueprint
 from api.blueprints.student_tutor_sessions.views import student_tutor_sessions_api_blueprint
+from api.blueprints.payments.views import payments_api_blueprint
 
 app.register_blueprint(csrf.exempt(students_api_blueprint), url_prefix='/api/v1/students')
 app.register_blueprint(csrf.exempt(tutors_api_blueprint), url_prefix='/api/v1/tutors')
 app.register_blueprint(csrf.exempt(login_api_blueprint), url_prefix='/api/v1/login')
 app.register_blueprint(csrf.exempt(subjects_api_blueprint), url_prefix='/api/v1/subjects')
 app.register_blueprint(csrf.exempt(tutor_sessions_api_blueprint), url_prefix='/api/v1/tutor_sessions')
-app.register_blueprint(csrf.exempt(student_tutor_sessions_api_blueprint), url_prefix='/api/v1/student_tutor_sessions')
+app.register_blueprint(csrf.exempt(payments_api_blueprint), url_prefix='/api/v1/payments')
