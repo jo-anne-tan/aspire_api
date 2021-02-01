64392137363435e15c4d802f25e717d270d01851
Syakir Wong <mohdsyakir9997@gmail.com>
2021-02-01 11:39:07 +0800

    add subjects api

--- preset_subjects.py
+++ preset_subjects.py
@@ -4,21 +4,11 @@
 # if there are no subjects in database yet,
 # create a list of preset subjects.
 
-# WIP : running python preset_subjects.py returns KeyError: 'DATABASE_URL'
-# Most likely caused by environment variables not loaded
-#   File "/home/joannetan/aspire_api/database.py", line 15, in return_db
-#     db_config = parse_db_url(os.environ['DATABASE_URL'])
-#   File "/home/joannetan/anaconda3/envs/ASPIRE/lib/python3.9/os.py", line 679, in __getitem__
-#     raise KeyError(key) from None
-
-
-
-from models.subject import Subject
-
 print("Loading environment variables from .env")
 from dotenv import load_dotenv
 load_dotenv()
 
+from models.subject import Subject
 
 exist_subjects = Subject.select()
 
