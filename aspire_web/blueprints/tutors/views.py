6e402c52cc6d6a06c23afae66a806d3550e26f54
Syakir Wong <mohdsyakir9997@gmail.com>
2021-02-05 18:00:17 +0800

    edit upload photo aspire_web

--- aspire_web/blueprints/tutors/views.py
+++ aspire_web/blueprints/tutors/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, flash, render_template,request,redirect,url_for,ses
 from models.tutor import Tutor
 import boto3, botocore
 from config import Config
+from api.util.helpers import upload_file_to_s3
+from werkzeug.utils import secure_filename
 
 S3_KEY=Config.S3_KEY
 S3_SECRET=Config.S3_SECRET
@@ -13,30 +15,30 @@ tutors_blueprint = Blueprint('tutors',
                             __name__,
                             template_folder='templates')
 
-@tutors_blueprint.route('/create')
-def create():
+# @tutors_blueprint.route('/create')
+# def create():
 
-    new_tutor = Tutor(
-        first_name="Jane",
-        last_name= "Doe",
-        age=34,
-        is_female= True,
-        email= 'ABC4@gmail.com',
-        password='ABC4@gmail.com',
-    )
+#     new_tutor = Tutor(
+#         first_name="Jane",
+#         last_name= "Doe",
+#         age=34,
+#         is_female= True,
+#         email= 'ABC4@gmail.com',
+#         password='ABC4@gmail.com',
+#     )
 
-    if new_tutor.save():
-        flash("Tutor created!")
-        
-        # updating username
-        username = new_tutor.first_name+new_tutor.last_name+str(new_tutor.id)
-        new_tutor.username=username
-        new_tutor.save()
+#     if new_tutor.save():
+#         flash("Tutor created!")
 
-        return redirect(url_for('home'))
-    else:
-        flash("Unable to create Tutor!")
-        return render_template('home.html', errors = new_tutor.errors)
+#         # updating username
+#         username = new_tutor.first_name+new_tutor.last_name+str(new_tutor.id)
+#         new_tutor.username=username
+#         new_tutor.save()
+
+#         return redirect(url_for('home'))
+#     else:
+#         flash("Unable to create Tutor!")
+#         return render_template('home.html', errors = new_tutor.errors)
 
 @tutors_blueprint.route('/profile-photo')
 def upload_profile():
@@ -44,30 +46,11 @@ def upload_profile():
 
 @tutors_blueprint.route('/profile-photo', methods=['POST'])
 def upload():
-    tutor = Tutor.get_by_id(2) #hard coded for testing purposes
-
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=S3_KEY,
-        aws_secret_access_key=S3_SECRET
-    )
+    tutor = Tutor.get_by_id(1) #hard coded for testing purposes
 
     file = request.files["profile_photo"]
-
-    image_path= tutor.username + "/profile-pic/"+ file.filename
-
-    s3.upload_fileobj(
-        file,
-        S3_BUCKET,
-        image_path,
-        ExtraArgs={
-            "ACL": "public-read",
-            "ContentType": file.content_type
-        }
-    )
-
-    file_loc = S3_LOCATION + image_path
-    flash('Image uploaded successfully.','info')
+    file.filename = secure_filename(file.filename)
+    image_path = upload_file_to_s3(file, "tutor", tutor.username)
 
     # save photo url in database
     tutor.profile_image=image_path
